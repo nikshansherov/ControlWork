051929c29cfd2f4fc946b610fb743f965a44c0cd
Nikolay <nikshansherov@yandex.ru>
2022-09-15 16:50:56 +0300

    Добавил функцию ввода массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+string[] CreateArray()
+{
+    Console.Write("Введите длину массива: ");
+    int lenArray = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[lenArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
